1287ea8ff33be9ae2063eae0d5b771fa42ce6372
Alex Gabor <alex.dan.gabor@gmail.com>
2021-05-05 15:27:33 +0300

    Try nested fling

--- DesignChallenges/CookingAppByLorenaPop/app/src/main/java/com/alexgabor/cookingapp_lorenapop/screen/recipe/RecipeScreen.kt
+++ DesignChallenges/CookingAppByLorenaPop/app/src/main/java/com/alexgabor/cookingapp_lorenapop/screen/recipe/RecipeScreen.kt
@@ -1,23 +1,30 @@
 package com.alexgabor.cookingapp_lorenapop.screen.recipe
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.calculateTargetValue
+import androidx.compose.animation.defaultDecayAnimationSpec
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.BackdropScaffold
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
@@ -28,6 +35,8 @@ import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
@@ -38,12 +47,29 @@ import com.alexgabor.cookingapp_lorenapop.catalog.SwipeButton
 import com.alexgabor.cookingapp_lorenapop.theme.AppTheme
 import com.google.accompanist.insets.navigationBarsPadding
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun RecipeScreen(navController: NavHostController) {
+    val minOffset = 0f
+    val maxOffset = with(LocalDensity.current) { (LocalConfiguration.current.screenWidthDp.dp - 24.dp).toPx() }
+    var currentOffset by remember { mutableStateOf(maxOffset) }
+    val listState = rememberLazyListState()
+    val flingDecay = defaultDecayAnimationSpec()
+
     val nestedScrollConnection = remember {
         object : NestedScrollConnection {
+
+            fun scrollBy(available: Float): Float {
+                if (available > 0 && (listState.firstVisibleItemIndex > 0 || listState.firstVisibleItemScrollOffset > 0)) return 0f
+                val availableEnd = (currentOffset + available)
+                currentOffset = availableEnd.coerceIn(minOffset, maxOffset)
+                return (available - (availableEnd - currentOffset)).also { println("zzz scrolledBy $it") }
+            }
+
             override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-                return super.onPreScroll(available, source)
+                println("zzz preScroll $available")
+                return Offset(x = 0f, y = scrollBy(available.y))
+//                return super.onPreScroll(available, source)
             }
 
             override fun onPostScroll(consumed: Offset, available: Offset, source: NestedScrollSource): Offset {
@@ -51,79 +77,112 @@ fun RecipeScreen(navController: NavHostController) {
             }
 
             override suspend fun onPreFling(available: Velocity): Velocity {
+                println("zzz preFling $available")
+//                if (currentOffset <= minOffset) return available
+
+//                val direction = if (currentOffset > minOffset) -1 else 1
+//                val velocity = available.y * direction
+//                if (velocity < 0 && currentOffset > minOffset || velocity > 0 && (listState.firstVisibleItemIndex == 0 && listState.firstVisibleItemScrollOffset == 0)) {
+//                    val animationResult = Animatable(currentOffset).apply {
+//                        updateBounds(minOffset, maxOffset)
+//                    }.animateDecay(velocity, flingDecay) {
+//                        println("zzz decay $value")
+//                        currentOffset = this.value
+//                    }
+//                    return available
+////                    return Velocity(x = 0f, available.y - animationResult.endState.velocity * direction).also {
+////                        println("zzz pre consumed $it = $velocity - ${animationResult.endState.velocity}")
+////                    }
+//                }
                 return super.onPreFling(available)
             }
 
             override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
-                return super.onPostFling(consumed, available)
+                println("zzz postFling $consumed $available ${listState.firstVisibleItemIndex} ${listState.firstVisibleItemScrollOffset}")
+//                val direction = if (currentOffset > minOffset) -1 else 1
+//                val velocity = available.y * direction
+////                if (velocity > 0 && (listState.firstVisibleItemIndex == 0 && listState.firstVisibleItemScrollOffset == 0)) {
+//                    val animationResult = Animatable(currentOffset).apply {
+//                        updateBounds(minOffset, maxOffset)
+//                    }.animateDecay(velocity, flingDecay) {
+//                        currentOffset = this.value
+//                    }
+//                    return Velocity.Zero
+//                    return Velocity(x = 0f, velocity - animationResult.endState.velocity)
+//                }
+                return available
             }
         }
     }
-    Box {
-        Column(Modifier
+    Box(Modifier
+        .fillMaxSize()
+        .nestedScroll(nestedScrollConnection)) {
+        Image(painter = painterResource(R.drawable.cookies), contentDescription = "Recipe Banner Imager", contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f))
+        LazyColumn(Modifier
+            .padding(top = with(LocalDensity.current) { currentOffset.toDp() })
             .fillMaxSize()
-            .nestedScroll(nestedScrollConnection)) {
-            Image(painter = painterResource(R.drawable.cookies), contentDescription = "Recipe Banner Imager", contentScale = ContentScale.Crop,
-                modifier = Modifier
+            .clip(AppTheme.shapes.bottomSheet)
+            .background(AppTheme.colors.surface),
+            state = listState,
+            contentPadding = PaddingValues(AppTheme.dimens.screenPadding)) {
+            item {
+                Row {
+                    Box(Modifier.weight(1f)) {
+                        Text(text = "Chocolate Chip Cookies", style = AppTheme.typography.titleTall, modifier = Modifier)
+                    }
+                    Image(painterResource(R.drawable.ic_heart),
+                        contentDescription = "Favourites",
+                        Modifier.padding(start = AppTheme.dimens.iconSpacing, end = AppTheme.dimens.iconSpacing))
+                }
+            }
+            item {
+                Row(Modifier.padding(top = 32.dp)) {
+                    Box(Modifier.weight(1f)) {
+                        Text(text = "Rating")
+                    }
+                    Text(text = "Review", style = AppTheme.typography.flatButton)
+                    Image(painter = painterResource(id = R.drawable.ic_plus),
+                        contentDescription = "Add Review",
+                        colorFilter = ColorFilter.tint(AppTheme.colors.accent),
+                        modifier = Modifier
+                            .padding(start = 4.dp)
+                            .size(12.dp)
+                            .align(CenterVertically))
+                }
+            }
+            item {
+                Row(modifier = Modifier
                     .fillMaxWidth()
-                    .aspectRatio(1f))
-            LazyColumn(Modifier
-                .offset(y = (-24).dp)
-                .fillMaxSize()
-                .clip(AppTheme.shapes.bottomSheet)
-                .background(AppTheme.colors.surface),
-                contentPadding = PaddingValues(AppTheme.dimens.screenPadding)) {
-                item {
-                    Row {
-                        Box(Modifier.weight(1f)) {
-                            Text(text = "Chocolate Chip Cookies", style = AppTheme.typography.titleTall, modifier = Modifier)
-                        }
-                        Image(painterResource(R.drawable.ic_heart),
-                            contentDescription = "Favourites",
-                            Modifier.padding(start = AppTheme.dimens.iconSpacing, end = AppTheme.dimens.iconSpacing))
-                    }
+                    .padding(top = 32.dp), horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                    Chip(painter = painterResource(id = R.drawable.ic_clock), text = "1.5 h")
+                    Chip(painter = painterResource(id = R.drawable.ic_fire), text = "365 kcal")
+                    Chip(painter = painterResource(id = R.drawable.ic_eat), text = "12 pc")
                 }
-                item {
-                    Row(Modifier.padding(top = 32.dp)) {
-                        Box(Modifier.weight(1f)) {
-                            Text(text = "Rating")
-                        }
-                        Text(text = "Review", style = AppTheme.typography.flatButton)
-                        Image(painter = painterResource(id = R.drawable.ic_plus),
-                            contentDescription = "Add Review",
-                            colorFilter = ColorFilter.tint(AppTheme.colors.accent),
-                            modifier = Modifier
-                                .padding(start = 4.dp)
-                                .size(12.dp)
-                                .align(CenterVertically))
-                    }
-                }
-                item {
-                    Row(modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 32.dp), horizontalArrangement = Arrangement.spacedBy(16.dp)) {
-                        Chip(painter = painterResource(id = R.drawable.ic_clock), text = "1.5 h")
-                        Chip(painter = painterResource(id = R.drawable.ic_fire), text = "365 kcal")
-                        Chip(painter = painterResource(id = R.drawable.ic_eat), text = "12 pc")
-                    }
-                }
-                item {
-                    Text(text = "To find the absolute best, we tested a ton of variables — various different kinds of sugar, fat, and flour — then put it all together to create the ultimate chocolate chip cookie.",
-                        style = AppTheme.typography.bodyNormalOnSurface,
-                        modifier = Modifier.padding(top = 32.dp))
-                }
-                item {
-                    Box(Modifier
-                        .navigationBarsPadding()) {
-                        Spacer(Modifier.size(AppTheme.dimens.buttonHeight))
-                    }
+            }
+            items(50) {
+                Text(text = "To find the absolute best, we tested a ton of variables — various different kinds of sugar, fat, and flour — then put it all together to create the ultimate chocolate chip cookie.",
+                    style = AppTheme.typography.bodyNormalOnSurface,
+                    modifier = Modifier.padding(top = 32.dp))
+            }
+            item {
+                Text(text = "To find the absolute best, we tested a ton of variables — various different kinds of sugar, fat, and flour — then put it all together to create the ultimate chocolate chip cookie.",
+                    style = AppTheme.typography.bodyNormalOnSurface,
+                    modifier = Modifier.padding(top = 32.dp))
+            }
+            item {
+                Box(Modifier
+                    .navigationBarsPadding()) {
+                    Spacer(Modifier.size(AppTheme.dimens.buttonHeight))
                 }
             }
         }
-        SwipeButton(modifier = Modifier
-            .fillMaxWidth()
-            .align(BottomCenter)
-            .padding(AppTheme.dimens.screenPadding)
-            .navigationBarsPadding())
+//        SwipeButton(modifier = Modifier
+//            .fillMaxWidth()
+//            .align(BottomCenter)
+//            .padding(AppTheme.dimens.screenPadding)
+//            .navigationBarsPadding())
     }
 }
