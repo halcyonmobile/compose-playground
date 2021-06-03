307ba33e03b87fb611f9412c802f98e89e210c6d
Alex Gabor <alex.dan.gabor@gmail.com>
2021-06-03 09:53:59 +0300

    Update dependencies in Components

--- Components/app/src/main/java/com/halcyonmobile/composecomponents/component/list_with_scroll_indicator/Carousel.kt
+++ Components/app/src/main/java/com/halcyonmobile/composecomponents/component/list_with_scroll_indicator/Carousel.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
